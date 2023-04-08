92e06821f424497cd77cc178f1c0015c845695d3
Peter Matthews <peter@pjmatthews.dev>
2023-04-08 12:05:09 -0500

    :sparkles: Basic Tailwind Page

---
+++ web-app/src/tailwind/script.tsx
@@ -0,0 +1,7 @@
+import { render } from "solid-js/web";
+
+const App = () => {
+	return <h1 class="underline text-9xl text-center mt-20">Hello, World!</h1>;
+};
+
+render(() => <App />, document.body);
